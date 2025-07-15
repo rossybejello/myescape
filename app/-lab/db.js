46b03e588ab748b746d15ab052d55bd915abcf87
rossybejello <rossybejello@gmail.com>
2025-07-15 12:41:12 +0000

    Delete app directory

--- app/-lab/db.js
+++
@@ -1,2 +0,0 @@
-import { Pool } from 'pg';
-export const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
--- app/-lab/serial.js
+++
@@ -1,8 +0,0 @@
-export function makeSerial() {
-  const alphabet = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
-  let code = '';
-  crypto.getRandomValues(new Uint8Array(10)).forEach(b => {
-    code += alphabet[b % alphabet.length];
-  });
-  return code;
-}
